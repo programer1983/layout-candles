381d6f0b1246969011682fafcad058de9b901999
programer1983 <programer972@gmail.com>
2024-02-22 20:18:40 +0200

    create questions  block  with mobile adaptation

--- js/main.js
+++ js/main.js
@@ -41,6 +41,23 @@ const swiper2 = new Swiper('.certificates-swiper', {
 
 /* ============================================================================ */
 
+const accordeon = document.querySelector('.questions-accordion')
+const accordeonTitles = accordeon.querySelectorAll('.questions-accordion__button')
+
+accordeonTitles.forEach((accordeonTitle) => {
+    accordeonTitle.addEventListener('click', function(){
+        const currentText = accordeonTitle.parentElement.querySelector('.questions-accordion__text')
+        accordeonTitle.classList.toggle('active')
+        currentText.classList.toggle('visible')
+
+        if (currentText.classList.contains('visible')){
+            currentText.style.maxHeight = currentText.scrollHeight + 'px'
+        }else{
+            currentText.style.maxHeight = null
+        }
+    })
+})
+
 
 
 
